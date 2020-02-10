f406a9b56e21e603d4298e89502ae5d4b1c98772
John Joseph Afaga <afagaj@spu.edu>
2020-02-09 17:57:57 -0800

    really minor

--- dialerwindow.cpp
+++ dialerwindow.cpp
@@ -1,11 +1,14 @@
 #include "dialerwindow.h"
 #include "ui_dialerwindow.h"
+#include "QLineEdit"
+
 
 DialerWindow::DialerWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::DialerWindow)
 {
     ui->setupUi(this);
+    ui->lineDisplay->setInputMask("(999) 999-9999");
 }
 
 DialerWindow::~DialerWindow()
