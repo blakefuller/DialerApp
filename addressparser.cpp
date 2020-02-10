a601b6b5d6dcba2c9abf266f40deccaee111cb40
blakefuller <bfuller3134@gmail.com>
2020-02-09 20:25:39 -0800

    actually added AddressParser

--- addressparser.cpp
+++ addressparser.cpp
@@ -1,6 +1,39 @@
 #include "addressparser.h"
+#include <QApplication>
+#include <QFile>
+#include <QTextStream>
+#include <QString>
+#include <QStandardItem>
 
 AddressParser::AddressParser()
 {
 
+}
+
+QStandardItemModel* AddressParser::Parse()
+{
+    QStandardItemModel *model = new QStandardItemModel;
+
+    QFile file("us-500.csv");
+    if(file.open(QIODevice::ReadOnly))
+    {
+        int lineindex = 0;
+        QTextStream in(&file);
+
+        while(!in.atEnd())
+        {
+            QString line = in.readLine();
+            QStringList lineToken = line.split(",", QString::SkipEmptyParts);
+
+            for(int i = 0; i < lineToken.size(); i++)
+            {
+                QString value = lineToken.at(i);
+                QStandardItem *item = new QStandardItem(value);
+                model->setItem(lineindex, i, item);
+            }
+            lineindex++;
+        }
+        file.close();
+    }
+    return model;
 }
--- addressparser.h
+++ addressparser.h
@@ -1,11 +1,12 @@
 #ifndef ADDRESSPARSER_H
 #define ADDRESSPARSER_H
-
+#include <QStandardItem>
 
 class AddressParser
 {
 public:
     AddressParser();
+    QStandardItemModel* Parse();
 };
 
 #endif // ADDRESSPARSER_H
